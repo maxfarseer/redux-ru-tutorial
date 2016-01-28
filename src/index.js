482e15b5140080d81bcb47a77e87d9d13fb93f39
Max Patsiansky <m.patsianskiy@zmeke.com>
2016-01-28 18:00:06 +0300

    reducer in work

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
-//import configureStore from './store/configureStore'
+import configureStore from './store/configureStore'
 
-//const store = configureStore()
+const store = configureStore()
 
-/*render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)*/
-render(<App />, document.getElementById('root'))
+)
