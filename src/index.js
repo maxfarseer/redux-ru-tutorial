ef0c5d38039e9af7555d7580e2e0ef11f683a8eb
Max P <maxfarseer@gmail.com>
2016-02-01 00:19:49 +0300

    add provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './containers/App'
+import configureStore from './store/configureStore'
 
+const store = configureStore()
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
