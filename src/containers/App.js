fd4f5a34d87cde98587e114f4ae91d80d5a33a9e
Max P <maxfarseer@gmail.com>
2016-02-07 16:29:57 +0300

    add reducer and play with it

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <div>Привет из App</div>
+    const { name, surname, age } = this.props.user
+    return <div>
+      <p>Привет из App, {name} {surname}!</p>
+      <p>Тебе уже {age} ?</p>
+    </div>
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    user: state
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  name: 'Василий',
+  surname: 'Реактов',
+  age: 27
+}
+
+export default function userstate(state = initialState) {
+  return state
+}
