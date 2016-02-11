0e873f6fc14d76c82c956d8a08d41a2dd5ea7e72
Max Patsiansky <m.patsianskiy@zmeke.com>
2016-02-11 18:33:20 +0300

    add enhancer - logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,14 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
+import createLogger from 'redux-logger'
+
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState)
+  const logger = createLogger()
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(logger))
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
